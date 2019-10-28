cde96ae389443f9c768738acfdcc27b855aa792f
Denis Yakovlev <denn.yakovlev@gmail.com>
2019-10-28 10:17:28 +0300

    init commit

---
+++ DataLayer.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.IO;
+
+namespace DatabaseManipulator
+{
+
+    class Record
+    {
+        /// <summary>
+        /// Класс, представляющий одну запись в БД
+        /// </summary>
+
+        public int Id { get; private set; }
+        public DateTime Datetime { get; private set; }
+        public double Value { get; private set; }
+
+        public Record(int id, DateTime dt, double val)
+        {
+            Id = id;
+            Datetime = dt;
+            Value = val;
+        }
+
+        /// <summary>
+        /// Cериализует объект
+        /// </summary>
+        /// <returns></returns>
+        public byte[] Serialize()
+        {
+            byte[] bytes = new byte[20];
+            BitConverter.GetBytes(Id).CopyTo(bytes, 0);
+            BitConverter.GetBytes(Datetime.ToBinary()).CopyTo(bytes, 4);
+            BitConverter.GetBytes(Value).CopyTo(bytes, 12);
+            return bytes;
+        }
+    }
+
+    interface IDto
+    {
+
+    }
+
+    static class DbInteractor
+    {
+        public static Database Database { get; private set; }
+        public static void Open(string path)
+        {
+            //
+        }
+        //public static bool Create(Record r)
+        //{
+        //    // 
+        //}
+        //public static bool Read(int id, out IDto responseDto)
+        //{
+        //    //
+        //}
+        //public static bool Update(int id, Record r)
+        //{
+        //    //
+        //}
+        //public static bool Delete(int id)
+        //{
+        //    //
+        //}
+    }
+
+    class Database
+    {
+        public Record[] Records { get; private set; }
+        public Database(Record[] records)
+        {
+            Records = records;
+        }
+        public Record this[int i]
+        {
+            get => Records[i];
+            set { Records[i] = value; }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DatabaseManipulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Record rec = new Record(1, DateTime.Parse("01.05.2016 04:15:29"), 0.92d);
+            byte[] ser = rec.Serialize();
+            //Console.WriteLine(BitConverter.GetBytes(rec.Value)); 
+            //long l1 = Convert.ToInt64(0.92d);
+            //long l2 = BitConverter.DoubleToInt64Bits(1.234);
+
+
+        }
+    }
+}
