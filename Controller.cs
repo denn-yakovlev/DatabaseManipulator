1ad01a41627e7bea6c421a71bc20dc8a5b36db39
Denis Yakovlev <denn.yakovlev@gmail.com>
2020-01-20 18:13:39 +0300

    Almost done but shit code
    WIP:  SolveProblem()

--- Controller.cs
+++ Controller.cs
@@ -36,15 +36,50 @@ namespace DatabaseManipulator
             return response.ResponseCode;
         }
 
-        public static ResponseCodes SolveProblem(int entryNum, double[] valuesSeq, out DateTime result)
+        public static List<Record> ReadAll()
+        {
+            return DatabaseInteractor.Database.Records;
+        }
+
+
+        // wrong
+
+        public static DateTime SolveProblem(int entryNum, double[] valuesSeq)
         {
             /*
-             Дана последовательность показаний некоего датчика во времени. 
-            Отобразить на экране время начала n-ого вхождения заданной последовательности 
-            (искомая последовательность может быть длиной до 5 символов). 
-            В измерениях «вхождения» последовательности могут повторяться не один раз;
+                   Дана последовательность показаний некоего датчика во времени. 
+                  Отобразить на экране время начала n-ого вхождения заданной последовательности 
+                  (искомая последовательность может быть длиной до 5 символов). 
+                  В измерениях «вхождения» последовательности могут повторяться не один раз;
             */
             throw new NotImplementedException();
+            List<Record> records = ReadAll();
+            DateTime dt = DateTime.MinValue;
+            int currEntry = 0;
+            for (int i = 0; i < records.Count - valuesSeq.Length; i++)
+            {
+                if (records[i].Value == valuesSeq[0])
+                {
+                    dt = records[i].Datetime;
+                    int j;
+                    for (j = 0; j < valuesSeq.Length; i++)
+                    {
+                        if (records[i + j].Value != valuesSeq[j])
+                        {
+                            break;
+                        }
+                    }
+                    if (j == valuesSeq.Length)
+                    {
+                        currEntry++;
+                    }
+                    if (currEntry == entryNum)
+                    {
+                        return dt;
+                    }
+                }
+            }
+            return dt;
         }
     }
 }
--- UserLayer.cs
+++ UserLayer.cs
@@ -7,7 +7,7 @@ namespace DatabaseManipulator
 {
     static class UserLayer
     {
-        public static void PrintMenu()
+        static void PrintMenu()
         {
             Console.WriteLine(@"1. Добавить запись в БД
 2. Прочитать запись
@@ -17,11 +17,22 @@ namespace DatabaseManipulator
 6. Решить задачу
 0. Выход");
         }
+
         public static void Interpret()
         {
             PrintMenu();
             Console.Write('>');
-            int choice = Console.Read() - '0';
+            int choice;
+            bool isParseOk;
+            do
+            {
+                isParseOk = int.TryParse(Console.ReadLine(), out choice);
+                if (!isParseOk)
+                {
+                    Console.WriteLine("Введите целое число!");
+                    Console.WriteLine();
+                }
+            } while (!isParseOk);
             while (choice != 0)
             {
                 if (choice < 1 || choice > 6)
@@ -67,26 +78,160 @@ namespace DatabaseManipulator
             }
             while (!isParseOk);
 
+            var nfi = new NumberFormatInfo();
+            nfi.NumberDecimalSeparator = ".";
+            double val;
+            Console.WriteLine("Введите значение (дробное число)");
+            do
+            {
+                line = Console.ReadLine();
+                isParseOk = double.TryParse(line, NumberStyles.AllowDecimalPoint, nfi, out val);
+                if (!isParseOk)
+                {
+                    Console.WriteLine("Неверный ввод, попробуйте ещё раз");
+                }
+            }
+            while (!isParseOk);
+            ResponseCodes respCode = Controller.CreateRecord(id, dt, val);
+            if (respCode == ResponseCodes.OK)
+            {
+                Console.WriteLine("OK");
+            }
+            else if (respCode == ResponseCodes.EXISTS_ID)
+            {
+                Console.WriteLine("Id уже существует");
+            }
+
+
         }
         static void ReadRecord()
         {
-            throw new NotImplementedException();
+            Console.WriteLine();
+            Console.WriteLine("Введите ID для записи (целое число)");
+            // wtf??
+            string line = Console.ReadLine();
+            int id = int.Parse(line);
+            DateTime dt;
+            double val;
+            ResponseCodes respCode = Controller.ReadRecord(id, out dt, out val);
+            if (respCode == ResponseCodes.OK)
+            {
+                Console.WriteLine($"{id}   {dt}   {val}");
+            }
+            else if (respCode == ResponseCodes.NO_ID)
+            {
+                Console.WriteLine("Такого ID нет в БД");
+            }
         }
         static void UpdateRecord()
         {
-            throw new NotImplementedException();
+            Console.WriteLine();
+            Console.WriteLine("Введите ID для записи (целое число)");
+            // wtf??
+            string line = Console.ReadLine();
+            int id = int.Parse(line);
+            Console.WriteLine("Введите новые дату и время (дд.ММ.гггг чч:мм:сс)");
+            bool isParseOk;
+            DateTime dt;
+
+            do
+            {
+                line = Console.ReadLine();
+                isParseOk = DateTime.TryParse(line, DateTimeFormatInfo.CurrentInfo, DateTimeStyles.None, out dt);
+                if (!isParseOk)
+                {
+                    Console.WriteLine("Неверный ввод, попробуйте ещё раз");
+                }
+            } while (!isParseOk);
+
+            var nfi = new NumberFormatInfo();
+            nfi.NumberDecimalSeparator = ".";
+            double val;
+            Console.WriteLine("Введите новое значение (дробное число)");
+            do
+            {
+                line = Console.ReadLine();
+                isParseOk = double.TryParse(line, NumberStyles.AllowDecimalPoint, nfi, out val);
+                if (!isParseOk)
+                {
+                    Console.WriteLine("Неверный ввод, попробуйте ещё раз");
+                }
+            }
+            while (!isParseOk);
+            ResponseCodes respCode = Controller.UpdateRecord(id, dt, val);
+            if (respCode == ResponseCodes.OK)
+            {
+                Console.WriteLine("OK");
+            }
+            else if (respCode == ResponseCodes.NO_ID)
+            {
+                Console.WriteLine("Id уже существует");
+            }
         }
         static void DeleteRecord()
         {
-            throw new NotImplementedException();
+            Console.WriteLine();
+            Console.WriteLine("Введите ID для записи (целое число)");
+            string line = Console.ReadLine();
+            int id = int.Parse(line);
+            ResponseCodes respCode = Controller.DeleteRecord(id);
+            if (respCode == ResponseCodes.OK)
+            {
+                Console.WriteLine($"ОК");
+            }
+            else if (respCode == ResponseCodes.NO_ID)
+            {
+                Console.WriteLine("Такого ID нет в БД");
+            }
         }
         static void ReadAll()
         {
-            throw new NotImplementedException();
+            var records = Controller.ReadAll();
+            foreach (var rec in records)
+            {
+                Console.WriteLine($"{rec.Id}   {rec.Datetime}   {rec.Value}");
+            }
         }
         static void SolveProblem()
         {
             throw new NotImplementedException();
+            string problemText = @"Дана последовательность показаний некоего датчика во времени. 
+Отобразить на экране время начала n-ого вхождения заданной последовательности 
+искомая последовательность может быть длиной до 5 символов). 
+В измерениях «вхождения» последовательности могут повторяться не один раз;
+";
+            Console.WriteLine(problemText);
+            Console.WriteLine("Введите номер вхождения (целое число)");
+            string line = Console.ReadLine();
+            int entryNum = int.Parse(line);
+            Console.WriteLine("Введите последовательность значений через пробел(дробное число)");
+            line = Console.ReadLine();
+            string[] split = line.Split(" ");
+            double d;
+            var nfi = new NumberFormatInfo();
+            nfi.NumberDecimalSeparator = ".";
+            List<double> valuesSeq = new List<double>();
+            foreach (string s in split)
+            {
+                bool isParseOk = double.TryParse(s, NumberStyles.AllowDecimalPoint, nfi, out d);
+                if (!isParseOk)
+                {
+                    Console.WriteLine("Неверный ввод, попробуйте ещё раз");
+                }
+                else
+                {
+                    valuesSeq.Add(d);
+                }
+            }
+            DateTime dt = Controller.SolveProblem(entryNum, valuesSeq.ToArray());
+            if (dt == DateTime.MinValue)
+            {
+                Console.WriteLine("Не найдено!");
+            }
+            else
+            {
+                Console.WriteLine($"OK: {dt}");
+            }
         }
     }
 }
